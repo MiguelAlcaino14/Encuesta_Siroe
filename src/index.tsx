7a88e51fd28ef7f4ee9e51735515b939c7a8042a
Miguel Alcaino <malcaino@siroe.cl>
2026-02-09 14:33:28 -0300

    Fix: Mover index.html e index.tsx a src y corregir rutas de build

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideZonelessChangeDetection } from '@angular/core';
 // CORRECCIÓN AQUÍ: Agregamos "/app" a la ruta
-import { AppComponent } from './src/app/app.component';
+import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
